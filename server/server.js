f07752fdc66578a8d1fed4189c1ee6dc005014c3
Valérie <valerie@Valerieventures.fritz.box>
2018-12-19 08:19:27 +0100

    Add mongoose configuration || Add POST /todos route

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('./db/mongoose');
+const Todo = require('./models/todo');
+const User = require('./models/user');
+
+const app = express();
+app.use(bodyParser.json());
+
+// C R U D
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({ text: req.body.text });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Server started on Port 3000.');
+});
