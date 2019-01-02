7afc37b054045bccd92b94bfb87418db904303e0
Valérie <valerie@Valerieventures.fritz.box>
2019-01-02 09:41:40 +0100

    Add /PATCH:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/todo');
@@ -66,6 +67,33 @@ app.delete('/todos/:id', (req, res) => {
     },
     err => res.status(400).send()
   );
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
